46e2fed03b91053667907c11f2467e07ddddd32e
Sai <saikakula123@gmail.com>
2021-06-12 04:09:26 +0530

    chore: add test cases

--- src/components/organisms/Header/Header.test.js
+++ src/components/organisms/Header/Header.test.js
@@ -2,13 +2,18 @@ import React from 'react'
 // Using render and screen from test-utils.js instead of
 // @testing-library/react
 import { render, screen } from '@testing-library/react'
-import HomePage from '../../src/pages/index'
+import '@testing-library/jest-dom'
+import Header from './Header'
+import { GlobalProvider } from '@contexts/GlobalContext'
 
-describe('HomePage', () => {
-  it('should render the heading', () => {
-    render(<HomePage />)
-
-    const heading = screen.getByText(
+describe('Header', () => {
+  it('should render the header', () => {
+    render(
+      <GlobalProvider>
+        <Header />
+      </GlobalProvider>
+    )
+    const div = screen.getByText(
       /Get It in time before the sale expires! 4 Days Left to Order/i
     )
 
